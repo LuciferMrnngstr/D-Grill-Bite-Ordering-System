47928f2acf030b470df1dceacfb298f48d68fdb6
LuciferMrnngstr <111272528+LuciferMrnngstr@users.noreply.github.com>
2023-03-01 13:59:59 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+    header('location: customer/home.php');
+?>
---
+++ script/app.js
@@ -0,0 +1,48 @@
+const navItems = document.querySelectorAll('.nav-items');
+
+navItems.forEach(navItem => {
+    navItem.addEventListener('click', () => {
+        navItems.forEach(navItem => {
+            navItem.classList.remove('active');
+        });
+
+        navItem.classList.add('active');
+    });
+});
+
+// food description activate
+const addToCartBtn = document.querySelector('.submit-btn-container #add-to-cart'),
+    descContainer = document.querySelector('.desc-container.open');
+
+if (addToCartBtn) {
+    addToCartBtn.addEventListener('click', () => {
+        descContainer.classList.add('active');
+    });
+}
+
+// increment and decrement quantity and update total price
+const inputQuantity = document.querySelector('.food-input-info #quantity'),
+    increaseBtn = document.querySelector('.food-input-info #increase'),
+    decreaseBtn = document.querySelector('.food-input-info #decrease'),
+    foodPrice = document.querySelector('.desc-container #food-price'),
+    subTotal = document.querySelector('.sub-total-cont #sub-total');
+
+if (inputQuantity) {
+    subTotal.innerHTML = foodPrice.innerHTML;
+
+    // Decrease quantity value
+    decreaseBtn.addEventListener('click', () => {
+        if (inputQuantity.value > 1 && inputQuantity.value <= 5) {
+            inputQuantity.value--;
+            subTotal.innerHTML = (foodPrice.innerHTML * inputQuantity.value).toFixed(2);
+        }
+    });
+
+    // Increae quantity value
+    increaseBtn.addEventListener('click', () => {
+        if (inputQuantity.value >= 1 && inputQuantity.value < 5) {
+            inputQuantity.value++;
+            subTotal.innerHTML = (foodPrice.innerHTML * inputQuantity.value).toFixed(2);
+        }
+    });
+}
